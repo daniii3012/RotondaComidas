063957fdea41d8b88cfaf6d7647e8b9b2c6e3a94
unknown <daniel_301999@hotmail.com>
2022-08-10 18:15:32 -0500

    Login

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../services/auth/auth.service';
 
 @Component({
   selector: 'app-navbar',
@@ -7,9 +8,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+  credentials: any;
+
+  userName: string;
+  password: string;
+
+  errorMsg: string;
+  showError: Boolean = false;
+
+  constructor(
+    public auth: AuthService
+  ) { }
 
   ngOnInit(): void {
+  }
+
+  login(userName: string, password: string){
+    this.credentials = {
+      "email" : userName,
+      "password" : password
+    }
+
+    this.auth.login(this.credentials).subscribe(
+      user => {
+        this.auth.setAuthState(user)
+        this.showError = false
+        console.log(user)
+      },
+      error => {
+        this.errorMsg = error
+        this.showError = true
+        this.password = ''
+      }
+    )
+  }
+
+  logout(){
+    this.auth.logout();
   }
 
 }
--- src/app/services/auth/auth.service.ts
+++ src/app/services/auth/auth.service.ts
@@ -1,9 +1,84 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
+import { throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+import { default as service } from '../services.json';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  private authState = JSON.parse(localStorage.getItem('authenticated')!) || false;
+
+  private user = JSON.parse(localStorage.getItem('user')!) || null;
+  private userToken = JSON.parse(localStorage.getItem('userToken')!) || null;
+  
+  /*
+  httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type':  'application/json',
+      Authorization: `Bearer ${service.token}`
+    })
+  };
+  */
+
+  constructor(private http: HttpClient) { }
+
+  login(credentials: any) {
+    return this.http.post(service.url + service.auth.endpoint, credentials).pipe(
+      catchError(this.handleError)
+    );
+  }
+
+  setAuthState(user: any) {
+    this.authState = true;
+    
+    this.user = user;
+    this.userToken = user.token;
+
+    localStorage.setItem('authenticated', 'true');
+    localStorage.setItem('user', JSON.stringify(user));
+    localStorage.setItem('userToken', JSON.stringify(user.token));
+  }
+
+  logout(){
+    this.authState = false;
+    
+    this.user = null;
+    this.userToken = null;
+
+    localStorage.removeItem('authenticated');
+    localStorage.removeItem('user');
+    localStorage.removeItem('userToken');
+  }
+
+  isLogged(){
+    return JSON.parse(localStorage.getItem('authenticated') || this.authState.toString());
+  }
+
+  
+  getUser(){
+    return this.user;
+  }
+
+  getUserToken(){
+    return this.userToken;
+  }
+
+  private handleError(error: HttpErrorResponse) {
+    if (error.error instanceof ErrorEvent) {
+      // A client-side or network error occurred. Handle it accordingly.
+      console.error('An error occurred:', error.error.message);
+    } else {
+      // The backend returned an unsuccessful response code.
+      // The response body may contain clues as to what went wrong,
+      console.error(
+        `Backend returned code ${error.status}, ` +
+        `body was: ${error.error}`);
+    }
+    // return an observable with a user-facing error message
+    return throwError(error.error);
+  };
+
 }
